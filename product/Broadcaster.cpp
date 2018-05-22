217f2198dc44c2b7fb7e100a3dfe7f4dc34e56b1
Cihan <cihankurt1998@gmail.com>
2018-05-22 13:51:52 +0200

    Mapstructure change + broadcast
    Broadcast.cpp eerste versie geimplementeerd. Mappen structuur is even aangepast.

---
+++ product/Broadcaster.cpp
@@ -0,0 +1,65 @@
+#include "Broadcaster.h"
+#include <unistd.h>
+#include <errno.h>
+
+void Broadcaster::SetIp(char *IP)
+{
+	remote_addr = IP;
+}
+
+int Broadcaster::SetUp()
+{
+	host = gethostbyname(remote_addr);
+	if (host == NULL)
+	{
+		return -1;
+	}
+
+	remoteServAddr.sin_family = host->h_addrtype;
+	memcpy((char *)&remoteServAddr.sin_addr.s_addr, host->h_addr_list[0], host->h_length);
+	remoteServAddr.sin_port = htons(REMOTE_SERVER_PORT);
+
+	return 1;
+}
+
+int Broadcaster::Bind()
+{
+	cliAddr.sin_family = AF_INET;
+	cliAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	cliAddr.sin_port = htons(0);
+
+	rc = bind(sd, (struct sockaddr *)&cliAddr, sizeof(cliAddr));
+	if (rc < 0)
+	{
+		close(sd);
+		return -1;
+	}
+	return 1;
+}
+
+int Broadcaster::Broadcast() //Let op dit is niet de constructor. Die staat onderaan.
+{
+	rc = sendto(sd, MESSAGEBUF, BUFSIZO, 0, (struct sockaddr *)&remoteServAddr, sizeof(remoteServAddr));
+
+	if (rc < 0)
+	{
+		close(sd);
+		return -1;
+	}
+	return 1;
+}
+
+Broadcaster::Broadcaster()
+{
+	strncpy(MESSAGEBUF, "Who is here?", bufferSize - 1); //laatste vrijhouden voor nullterminator
+	Message[bufferSize - 1] = '\0';
+	broadcast = 1;
+
+	if (SetUp() == -1 || Bind() == -1 || BroadCast() == -1)
+	{
+		perror("The setup/Bind went wrong: ");
+	}
+
+}
+
+//format cntrl shift i
--- product/Broadcaster.h
+++ product/Broadcaster.h
@@ -10,9 +10,14 @@ class Broadcaster
   public:
     Broadcaster();
     void SetIP(char *IP);
-    void Broadcast();
+  
 
   private:
+  int SetUp();
+  int Bind();
+  int Broadcast();
+
+
     int sockDescriptor;
     bool loopContinue;
     char Message[buffersize];
@@ -20,7 +25,7 @@ class Broadcaster
     struct sockaddr_in printerAddress;
     struct sockaddr_in hostAddress;
     struct hostent *host;
-    int broadcast;
+    int broadcast; 
 };
 
 #endif
